78b06bd17cab7a61e6a0769500a49b938d37ac93
Stanislau Pranovich <stanislau.pranovich@gmail.com>
2020-10-05 20:52:14 +0300

    feat: add task solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(members == null && members == undefined) return false;
+  let arr2 = [];
+  for(i = 0; i < members.length; i++) {
+    if(typeof(members[i]) === 'string') {
+      let x = members[i].trim().toUpperCase();
+          arr2.push(x[0]);
+          arr2.sort();
+    } 
+  };
+  return arr2.join("");
+}
