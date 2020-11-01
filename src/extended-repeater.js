3a4cda2e1c7a45169a6528d2af6bac83ba760f15
Stanislau Pranovich <stanislau.pranovich@gmail.com>
2020-11-01 13:20:19 +0300

    feat: add task solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str = '', options) {
+  let string = str,
+      result = '',
+      repTimes = options.repeatTimes || 1,
+      separator = options.separator || '+',
+      addition = options.addition || '',
+      addRepTimes = options.additionRepeatTimes || 1,
+      addSeparator = options.additionSeparator || '',
+      countRep = 1,
+      countAddRep = 0;
+
+  if (options.addition === false) addition = 'false';
+  if (options.addition === null) addition = 'null';
+
+
+  for (let i = 0; i < repTimes; i++) {
+    result += `${String(string)}`;
+    for (let j = 0; j < addRepTimes; j++) {
+      result += `${String(addition)}`;
+      countAddRep++;
+      if (addRepTimes === countAddRep) {
+        continue;
+      } else {
+        result += `${String(addSeparator)}`;
+      }
+    }
+    if (repTimes === countRep) {
+      continue;
+    } else {
+      result += `${String(separator)}`;
+      countRep++;
+    }
+    countAddRep = 0;
+  }
+
+  return result;
 };
   
